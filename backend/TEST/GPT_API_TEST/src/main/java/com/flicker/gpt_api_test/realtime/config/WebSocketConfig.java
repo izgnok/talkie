8f496cdb8ffb7bcb324f87a03ff7649485d38c39
rinch12332 <rinch12332@gmail.com>
2024-10-24 15:04:50 +0900

    [feat] realtime API 테스트

---
+++ backend/TEST/GPT_API_TEST/src/main/java/com/flicker/gpt_api_test/realtime/config/WebSocketConfig.java
@@ -0,0 +1,23 @@
+package com.flicker.gpt_api_test.realtime.config;
+
+import com.flicker.gpt_api_test.realtime.service.ChatWebSocketHandler;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    private final ChatWebSocketHandler webSocketHandler;
+
+    public WebSocketConfig(ChatWebSocketHandler webSocketHandler) {
+        this.webSocketHandler = webSocketHandler;
+    }
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(webSocketHandler, "/ws/chat").setAllowedOrigins("*");
+    }
+}
