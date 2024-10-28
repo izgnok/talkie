ff5abdd565c05ff024e7b5a9d651beb962b07d29
rinch12332 <rinch12332@gmail.com>
2024-10-28 12:03:11 +0900

    MQTT TEST

---
+++ backend/TEST/MQTT_TEST/src/main/java/com/e104/mqtt_test/MqttTestApplication.java
@@ -0,0 +1,13 @@
+package com.e104.mqtt_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MqttTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MqttTestApplication.class, args);
+    }
+
+}
