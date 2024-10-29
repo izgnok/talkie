24421fa46a8848903f3f8f27cc87c317b5528b51
rinch12332 <rinch12332@gmail.com>
2024-10-29 10:21:46 +0900

    GPT 4 API 테스트

---
+++ backend/TEST/OPEN_AI_4_TEST/src/main/java/com/e104/open_ai_4_test/OpenAi4TestApplication.java
@@ -0,0 +1,13 @@
+package com.e104.open_ai_4_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenAi4TestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenAi4TestApplication.class, args);
+    }
+
+}
