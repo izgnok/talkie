60c5b41e93b52f34713a1b69706bf38579391787
조현정 <whguswjd456@naver.com>
2024-10-29 14:49:14 +0900

    [chore] Apply Suncheon font and update webpage title

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundPosition: {
+        "center-top": "center top",
+      },
+    },
   },
   plugins: [],
 };
