cdc46132ba1877d3105e97444e80528a8c281fc3
서정운 <sju9417@gmail.com>
2024-10-30 09:27:37 +0900

    [refactor] payload 추상화
    - 내부 구현은 아직 없음. 명세서 확실해지면 그에 맞춰서 수정 필요

---
+++ backend/RealTime/src/main/java/com/e104/realtime/mqtt/dto/MqttConversationEndDto.java
@@ -0,0 +1,7 @@
+package com.e104.realtime.mqtt.dto;
+
+public class MqttConversationEndDto {
+    public Integer getUserSeq() {
+        return null;
+    }
+}
---
+++ backend/RealTime/src/main/java/com/e104/realtime/mqtt/dto/MqttMessageSendDto.java
@@ -0,0 +1,11 @@
+package com.e104.realtime.mqtt.dto;
+
+public class MqttMessageSendDto {
+    public Integer getUserSeq() {
+        return null;
+    }
+
+    public String getContent() {
+        return null;
+    }
+}
