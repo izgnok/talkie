503209a476157cb4d4123aced4267624f6d1e2fd
서정운 <sju9417@gmail.com>
2024-11-01 10:42:05 +0900

    [fix] 미구현된 부분 마저 구현 및 config 수정

--- backend/RealTime/src/main/java/com/e104/realtime/mqtt/dto/MqttConversationEndDto.java
+++ backend/RealTime/src/main/java/com/e104/realtime/mqtt/dto/MqttConversationEndDto.java
@@ -1,7 +1,4 @@
 package com.e104.realtime.mqtt.dto;
 
-public class MqttConversationEndDto {
-    public Integer getUserSeq() {
-        return null;
-    }
+public record MqttConversationEndDto(int userSeq) {
 }
--- backend/RealTime/src/main/java/com/e104/realtime/mqtt/dto/MqttWebsocketConnectDto.java
+++ backend/RealTime/src/main/java/com/e104/realtime/mqtt/dto/MqttWebsocketConnectDto.java
@@ -1,7 +1,4 @@
 package com.e104.realtime.mqtt.dto;
 
-public class MqttWebsocketConnectDto {
-    public Integer getUserSeq() {
-        return null;
-    }
+public record MqttWebsocketConnectDto(int userSeq) {
 }
