238c3d2d54e025ad00ec8c0e445efcecd593323e
rinch12332 <rinch12332@gmail.com>
2024-11-01 12:37:30 +0900

    [feat] FastAPI DTO

---
+++ backend/RealTime/src/main/java/com/e104/realtime/dto/FastApiSentimentResponse.java
@@ -0,0 +1,20 @@
+package com.e104.realtime.dto;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class FastApiSentimentResponse {
+    private int happyScore;
+
+    private int loveScore;
+
+    private int sadScore;
+
+    private int scaryScore;
+
+    private int angryScore;
+
+    private int amazingScore;
+}
