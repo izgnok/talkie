b0d6b595cb95dd71f8fec7d6e8521ad730b9a33e
서정운 <sju9417@gmail.com>
2024-11-01 10:42:05 +0900

    [fix] 미구현된 부분 마저 구현 및 config 수정

--- backend/RealTime/src/main/java/com/e104/realtime/mqtt/dto/MqttVoiceRecognitionDto.java
+++ backend/RealTime/src/main/java/com/e104/realtime/mqtt/dto/MqttVoiceRecognitionDto.java
@@ -1,7 +1,4 @@
 package com.e104.realtime.mqtt.dto;
 
-public class MqttVoiceRecognitionDto {
-    public int getUserSeq() {
-        return 0;
-    }
+public record MqttVoiceRecognitionDto(int userSeq) {
 }
