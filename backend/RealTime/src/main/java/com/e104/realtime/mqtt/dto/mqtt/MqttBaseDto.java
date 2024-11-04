4c2fd611dafb5d0ca3928252bea149b6b72fbf85
서정운 <sju9417@gmail.com>
2024-11-04 09:23:42 +0900

    [chore] import 최적화

--- backend/RealTime/src/main/java/com/e104/realtime/mqtt/dto/mqtt/MqttBaseDto.java
+++ backend/RealTime/src/main/java/com/e104/realtime/mqtt/dto/mqtt/MqttBaseDto.java
@@ -1,7 +1,5 @@
 package com.e104.realtime.mqtt.dto.mqtt;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
 import java.util.HashMap;
 
 public record MqttBaseDto(int userSeq, String header, HashMap<String, String> data) {
