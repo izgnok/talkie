94ea2f73a5fb1ca2387113e2461d4c495f40a458
서정운 <sju9417@gmail.com>
2024-11-04 09:23:42 +0900

    [chore] import 최적화

--- backend/RealTime/src/test/java/com/e104/realtime/mqtt/dto/mqtt/MqttBaseDtoTest.java
+++ backend/RealTime/src/test/java/com/e104/realtime/mqtt/dto/mqtt/MqttBaseDtoTest.java
@@ -4,8 +4,6 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class MqttBaseDtoTest {
 
     ObjectMapper objectMapper = new ObjectMapper();
