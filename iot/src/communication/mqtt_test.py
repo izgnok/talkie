e02367386cf3b4a865103501ed09940a3d65b2f1
김하휼 <wlwhssla1034@naver.com>
2024-11-05 14:13:00 +0900

    [Feat] MQTT test with iot and backend server

--- iot/src/communication/mqtt_test.py
+++ iot/src/communication/mqtt_test.py
@@ -5,8 +5,9 @@ import os
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 from src.config.settings import BROKER_ADDRESS, TOPIC_SUB, TOPIC_PUB, CLIENT_ID, PROTOCOL
-from src.stt.stt_handler import start_conversation
+# from src.stt.stt_handler import start_conversation
 
+# 메시지 수신 콜백 함수
 # 서버에서 메시지를 수신할 때 호출됩니다.
 def on_message(client, userdata, message):
     print("B: " + message.payload.decode())
@@ -30,42 +31,15 @@ client.loop_start()
 
 user_seq = 1
 
-def publish_message(header, data=None):
-    msg = {
-        "userSeq": user_seq,
-        "header": header
-    }
-    if data is not None:
-        msg["data"] = data
-    client.publish(TOPIC_PUB, json.dumps(msg), qos=0)
-
-
-# 각 요청에 맞는 메시지 발행
-# WebSocket 연결 요청
-# publish_message("topic/websocket/connect")
-
-# 메시지 전송 요청
-publish_message("topic/message/send", data={"content": start_conversation()})
-
-# 대화 종료 요청
-# publish_message("topic/conversation/end")
-
-# # 사용자 감지 요청
-# publish_message("topic/user/detection")
-
-# # 음성 인식 요청
-# publish_message("topic/voice/recognition")
-
 # msg = start_conversation()
 # client.publish(TOPIC_PUB, msg, qos=0)
 
-# 이벤트 루프 중지 및 브로커 연결 종료
 try:
     while True:
-        pass  # 무한 루프를 사용하여 프로그램이 종료되지 않도록 유지
+        pass
 except KeyboardInterrupt:
-    print("프로그램 종료")
+    print("루프 종료")
 
-# 이벤트 루프 중지 및 연결 종료
-client.loop_stop()
-client.disconnect()
+# 이벤트 루프 중지 및 브로커 연결 종료
+client.loop_stop()  # 비동기 루프 중지
+client.disconnect()  # 연결 종료
