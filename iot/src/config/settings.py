af0425f1c6ce86156c6e0b8ffa307345966b0255
김하휼 <wlwhssla1034@naver.com>
2024-11-05 14:13:00 +0900

    [Feat] MQTT test with iot and backend server

--- iot/src/config/settings.py
+++ iot/src/config/settings.py
@@ -1,7 +1,8 @@
 # settings.py
 
+
 BROKER_ADDRESS = "k11e104.p.ssafy.io"
-TOPIC_SUB = "chat/b"
-TOPIC_PUB = "chat/a"
+TOPIC_SUB = "tokie-client"
+TOPIC_PUB = "tokie-server"
 CLIENT_ID = "Participant_A"
 PROTOCOL = "MQTTv311"
