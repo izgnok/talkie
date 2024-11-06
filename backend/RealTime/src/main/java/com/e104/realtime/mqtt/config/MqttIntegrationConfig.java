9f56134986f50293a8f1bb254058775e10b6a3fa
서정운 <sju9417@gmail.com>
2024-11-06 12:09:14 +0900

    [chore] import 최적화 및 final 설정

--- backend/RealTime/src/main/java/com/e104/realtime/mqtt/config/MqttIntegrationConfig.java
+++ backend/RealTime/src/main/java/com/e104/realtime/mqtt/config/MqttIntegrationConfig.java
@@ -11,7 +11,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.integration.annotation.IntegrationComponentScan;
 import org.springframework.integration.annotation.ServiceActivator;
-import org.springframework.integration.channel.DirectChannel;
 import org.springframework.integration.channel.QueueChannel;
 import org.springframework.integration.config.EnableIntegration;
 import org.springframework.integration.endpoint.MessageProducerSupport;
