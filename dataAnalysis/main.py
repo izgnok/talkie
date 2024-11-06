6311fab62526470009cb24f900582e95e4a64aee
이재찬 <jaechanjj@naver.com>
2024-11-06 10:04:41 +0900

    [fix] dto list로 변경

--- dataAnalysis/main.py
+++ dataAnalysis/main.py
@@ -46,17 +46,12 @@ class KOTEtagger(pl.LightningModule):
 
 # 모델 초기화 및 로드
 trained_model = KOTEtagger()
-trained_model.load_state_dict(torch.load("/app/kote_pytorch_lightning.bin"), strict=False)
-# trained_model.load_state_dict(torch.load(r"C:\Users\SSAFY\Desktop\kote_pytorch_lightning.bin"), strict=False)
+trained_model.load_state_dict(torch.load(r"C:\Users\SSAFY\Downloads\kote_pytorch_lightning.bin"), strict=False)
 trained_model.eval()
 
 # FastAPI 초기화
 app = FastAPI()
 
-
-# 요청 데이터 모델
-class TextRequest(BaseModel):
-    text: str
 
 # 요청 데이터 모델
 class TextListRequest(BaseModel):
@@ -69,18 +64,22 @@ class EmotionResponse(BaseModel):
 class MorphAnalyzeResponse(BaseModel):
     morph_analyze: float
 
+class WordCloudItem(BaseModel):
+    word: str
+    count: int
+
 class WordCloudResponse(BaseModel):
-    word_cloud: List[Tuple[str,int]];
+    wordCloud: List[WordCloudItem]
 
 
 # 감정 카테고리 매핑
 category_mapping = {
-    '기쁨점수': ['환영/호의', '감동/감탄', '고마움', '뿌듯함', '편안/쾌적', '즐거움/신남', '깨달음', '행복', '기쁨', '안심/신뢰'],
-    '사랑스러움점수': ['존경', '기대감', '아껴주는', '흐뭇함(귀여움/예쁨)'],
-    '슬픔점수': ['슬픔', '안타까움/실망', '절망', '패배/자기혐오', '힘듦/지침', '서러움', '불쌍함/연민'],
-    '두려움점수': ['공포/무서움', '불안/걱정', '당황/난처', '경악'],
-    '분노점수': ['불평/불만', '지긋지긋', '화남/분노', '우쭐댐/무시함', '의심/불신', '짜증', '어이없음', '한심함', '역겨움/징그러움', '귀찮음', '증오/혐오'],
-    '놀라움점수': ['신기함/관심', '놀람']
+    'happyScore': ['환영/호의', '감동/감탄', '고마움', '뿌듯함', '편안/쾌적', '즐거움/신남', '깨달음', '행복', '기쁨', '안심/신뢰'],
+    'loveScore': ['존경', '기대감', '아껴주는', '흐뭇함(귀여움/예쁨)'],
+    'sadScore': ['슬픔', '안타까움/실망', '절망', '패배/자기혐오', '힘듦/지침', '서러움', '불쌍함/연민'],
+    'scaryScore': ['공포/무서움', '불안/걱정', '당황/난처', '경악'],
+    'angryScore': ['불평/불만', '지긋지긋', '화남/분노', '우쭐댐/무시함', '의심/불신', '짜증', '어이없음', '한심함', '역겨움/징그러움', '귀찮음', '증오/혐오'],
+    'amazingScore': ['신기함/관심', '놀람']
 }
 
 
@@ -89,12 +88,12 @@ def map_emotion_scores(emotion_preds):
 
     # 각 주요 감정 카테고리의 점수를 초기화
     category_scores = {
-        '기쁨점수': 0,
-        '사랑스러움점수': 0,
-        '슬픔점수': 0,
-        '두려움점수': 0,
-        '분노점수': 0,
-        '놀라움점수': 0
+        'happyScore': 0,
+        'loveScore': 0,
+        'sadScore': 0,
+        'scaryScore': 0,
+        'angryScore': 0,
+        'amazingScore': 0
     }
 
     # 각 감정 레이블을 주요 감정 카테고리에 합산
@@ -108,30 +107,36 @@ def map_emotion_scores(emotion_preds):
 
 @app.post("/api/data/emotion", response_model=EmotionResponse)
 async def predict_emotion(request: TextListRequest):
-
     texts = request.textList
 
     mapped_scores = {
-        '기쁨점수': 0,
-        '사랑스러움점수': 0,
-        '슬픔점수': 0,
-        '두려움점수': 0,
-        '분노점수': 0,
-        '놀라움점수': 0
+        'happyScore': 0,
+        'loveScore': 0,
+        'sadScore': 0,
+        'scaryScore': 0,
+        'angryScore': 0,
+        'amazingScore': 0
     }
 
-    # 각 텍스트에 대한 감정 예측 수행 및 합산
-    for text in texts:
+    # 배치 사이즈 설정
+    batch_size = 10
+
+    # 텍스트를 배치로 나누기
+    for i in range(0, len(texts), batch_size):
+        batch_texts = texts[i:i + batch_size]  # 현재 배치 텍스트
+
+        # 각 텍스트에 대한 감정 예측 수행
         with torch.no_grad():
-            preds = trained_model(text)[0]
-            emotion_preds = {label: float(pred) for label, pred in zip(LABELS, preds)}
+            preds = trained_model(batch_texts)  # 배치 전체를 모델에 전달
+            for pred in preds:
+                emotion_preds = {label: float(pred_val) for label, pred_val in zip(LABELS, pred)}
 
-        filtered_emotion_preds = {label: score for label, score in emotion_preds.items() if score > 0.4}
-        mapped_score = map_emotion_scores(filtered_emotion_preds)
+                filtered_emotion_preds = {label: score for label, score in emotion_preds.items() if score > 0.4}
+                mapped_score = map_emotion_scores(filtered_emotion_preds)
 
-        # mapped_score 값을 mapped_scores에 누적
-        for key in mapped_scores.keys():
-            mapped_scores[key] += mapped_score.get(key, 0)
+                # mapped_score 값을 mapped_scores에 누적
+                for key in mapped_scores.keys():
+                    mapped_scores[key] += mapped_score.get(key, 0)
 
     # 모든 점수의 합 구하기
     total = sum(mapped_scores.values())
@@ -151,31 +156,42 @@ async def predict_emotion(request: TextListRequest):
     return EmotionResponse(predictions=mapped_scores)
 
 
+
 @app.post("/api/data/vocabulary",response_model=MorphAnalyzeResponse)
-async def morphAnalyze(request: TextRequest):
+async def morphAnalyze(request: TextListRequest):
 
-    text = request.text
+    text = request.textList
+    N = len(text)
+
     okt = Okt()
-    sentences = re.split(r'[.!?]', text)
 
-    if len(sentences) == 0:
-        ans = 0  # 텍스트가 비어 있을 경우 평균 형태소 수를 0으로 설정
+    if len(text) == 0:
+        ans = -1  # 텍스트가 비어 있을 경우 평균 형태소 수를 0으로 설정
     else:
-        ans = sum(len(okt.morphs(sentence)) for sentence in sentences) / len(sentences)
+        ans = sum(len(okt.morphs(te)) for te in text) / N
 
     return MorphAnalyzeResponse(morph_analyze=ans)
 
-@app.post("/api/data/wordcloud",response_model=WordCloudResponse)
-async def wordcloud(request: TextRequest):
+
+@app.post("/api/data/wordcloud", response_model=WordCloudResponse)
+async def wordcloud(request: TextListRequest):
+
     okt = Okt()
-    text = request.text
+    text = request.textList
+
     # 명사 추출 및 불용어 제거
-    words = okt.nouns(text)
+    words = []
+
+    for te in text:
+        words.extend(okt.nouns(te))
+
     stop_words = ['제', '저']
     filtered_words = [word for word in words if word not in stop_words]
 
-    # 단어 빈도 계산 후 리스트 변환
+    # 단어 빈도 계산
     word_counts = Counter(filtered_words)
-    word_counts_list = list(word_counts.items())
 
-    return WordCloudResponse(word_cloud=word_counts_list)
+    # 단어와 빈도수의 리스트를 WordCloudItem 객체 리스트로 변환
+    word_counts_list = [WordCloudItem(word=word, count=count) for word, count in word_counts.items()]
+
+    return WordCloudResponse(wordCloud=word_counts_list)
