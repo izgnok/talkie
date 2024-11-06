e41b36ce39d72b859375585ff8a3429807a60407
rinch12332 <rinch12332@gmail.com>
2024-11-06 17:55:42 +0900

    [fix] cors 설정 수정

--- backend/RealTime/src/main/java/com/e104/realtime/config/WebConfig.java
+++ backend/RealTime/src/main/java/com/e104/realtime/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("https://k11e104.p.ssafy.io", "http://k11e104.p.ssafy.io") // 여러 도메인 허용
+                .allowedOrigins("https://k11e104.p.ssafy.io", "http://k11e104.p.ssafy.io", "http://localhost:5173") // 여러 도메인 허용
                 .allowedMethods("POST", "PUT", "GET", "DELETE")
                 .allowCredentials(true); // 자격 증명 허용
     }
