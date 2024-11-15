1e2f66453835588cded699c1e1224223131024bc
조현정 <whguswjd456@naver.com>
2024-11-15 16:07:20 +0900

    [refactor] hide hamburger button when user's logged out

--- frontend/src/components/HamburgerButton.tsx
+++ frontend/src/components/HamburgerButton.tsx
@@ -7,10 +7,10 @@ import AlertModal from "./AlertModal";
 import calendarIcon from "/assets/hamburger/calendar.png";
 import modiInfoIcon from "/assets/hamburger/modiInfo.png";
 import logoutIcon from "/assets/hamburger/logout.png";
-import loginIcon from "/assets/hamburger/login.png";
+// import loginIcon from "/assets/hamburger/login.png";
 import { logout } from "../apis/api";
 import useUserStore from "../store/useUserStore";
-import { AnimatePresence } from "framer-motion"; // **AnimatePresence import 추가**
+import { AnimatePresence } from "framer-motion";
 
 const HamburgerButton: React.FC = () => {
   const [showMenu, setShowMenu] = useState(false);
@@ -79,67 +79,53 @@ const HamburgerButton: React.FC = () => {
 
   return (
     <>
-      {/* 햄버거 메뉴 버튼 */}
-      <button
-        onClick={toggleMenu}
-        className="fixed bottom-36 right-10 bg-white rounded-full shadow-lg cursor-pointer flex items-center justify-center w-20 h-20 z-50 group transition-transform transform hover:-translate-y-0.5"
-      >
-        <RxHamburgerMenu size={40} color="black" />
-      </button>
+      {/* 로그인 상태에서만 햄버거 버튼 표시 */}
+      {!isLoggedOut && (
+        <button
+          onClick={toggleMenu}
+          className="fixed bottom-36 right-10 bg-white rounded-full shadow-lg cursor-pointer flex items-center justify-center w-20 h-20 z-50 group transition-transform transform hover:-translate-y-0.5"
+        >
+          <RxHamburgerMenu size={40} color="black" />
+        </button>
+      )}
 
       {/* 햄버거 메뉴 */}
-      {showMenu && (
+      {showMenu && !isLoggedOut && (
         <div
           ref={menuRef}
           className={`fixed bottom-[250px] right-10 w-[83px] bg-white rounded-lg shadow-lg px-3 py-6 flex flex-col items-center z-20 transition-transform transition-opacity duration-300 ease-out transform origin-bottom ${
             isAnimating ? "scale-y-100 opacity-100" : "scale-y-0 opacity-0"
           }`}
         >
-          {isLoggedOut ? (
-            <button
-              onClick={() => navigate("/login")}
-              className="flex flex-col items-center transform transition-transform duration-200 hover:scale-105"
-            >
-              <img src={loginIcon} alt="로그인" className="w-11 h-10 mb-2" />
-              <span className="text-[13px] font-bold">로그인</span>
-            </button>
-          ) : (
-            <>
-              <button
-                onClick={openCalendar}
-                className="flex flex-col items-center mb-5 transform transition-transform duration-200 hover:scale-105"
-              >
-                <img
-                  src={calendarIcon}
-                  alt="날짜 이동"
-                  className="w-11 h-11 mb-2"
-                />
-                <span className="text-[13px] font-bold">날짜 이동</span>
-              </button>
-              <button
-                onClick={() => navigate("/info")}
-                className="flex flex-col items-center mb-5 transform transition-transform duration-200 hover:scale-105"
-              >
-                <img
-                  src={modiInfoIcon}
-                  alt="정보 수정"
-                  className="w-11 h-11 mb-2"
-                />
-                <span className="text-[13px] font-bold">정보 수정</span>
-              </button>
-              <button
-                onClick={handleLogout}
-                className="flex flex-col items-center transform transition-transform duration-200 hover:scale-105"
-              >
-                <img
-                  src={logoutIcon}
-                  alt="로그아웃"
-                  className="w-11 h-10 mb-2"
-                />
-                <span className="text-[13px] font-bold">로그아웃</span>
-              </button>
-            </>
-          )}
+          <button
+            onClick={openCalendar}
+            className="flex flex-col items-center mb-5 transform transition-transform duration-200 hover:scale-105"
+          >
+            <img
+              src={calendarIcon}
+              alt="날짜 이동"
+              className="w-11 h-11 mb-2"
+            />
+            <span className="text-[13px] font-bold">날짜 이동</span>
+          </button>
+          <button
+            onClick={() => navigate("/info")}
+            className="flex flex-col items-center mb-5 transform transition-transform duration-200 hover:scale-105"
+          >
+            <img
+              src={modiInfoIcon}
+              alt="정보 수정"
+              className="w-11 h-11 mb-2"
+            />
+            <span className="text-[13px] font-bold">정보 수정</span>
+          </button>
+          <button
+            onClick={handleLogout}
+            className="flex flex-col items-center transform transition-transform duration-200 hover:scale-105"
+          >
+            <img src={logoutIcon} alt="로그아웃" className="w-11 h-10 mb-2" />
+            <span className="text-[13px] font-bold">로그아웃</span>
+          </button>
           <div className="absolute -bottom-1.5 w-3 h-4 bg-white transform rotate-45"></div>
         </div>
       )}
