12f97f8da39dabcc96fc2fc64e17a976e69d099b
wodyddldl333 <wodyddldl333@naver.com>
2024-11-18 10:45:09 +0900

    [refator] logger 출력 코드 추가

--- iot/src/main.py
+++ iot/src/main.py
@@ -17,6 +17,9 @@ from src.sensors.pir_controll_wpy import detect_child_approach
 from src.stt.mqtt_for_stt import speech_to_text 
 from src.config.settings import BROKER_ADDRESS, TOPIC_SUB, TOPIC_PUB, CLIENT_ID, PROTOCOL
 from wake_word.wake_word_for_stt import detect_wake_word
+from src.logger.logger import get_logger
+
+logger = get_logger()
 
 # 이벤트 객체 생성
 voice_event = threading.Event()
@@ -32,7 +35,7 @@ message_queue = Queue()
 def on_disconnect(client, userdata, rc):
     global session_active
     if rc != 0:
-        print(f"비정상적으로 연결이 끊겼습니다. 재연결을 시도합니다. (rc: {rc})")
+        logger.warn(f"비정상적으로 연결이 끊겼습니다. 재연결을 시도합니다. (rc: {rc})")
         session_active = False  # 세션이 끊겼음을 표시
 
         # 재연결 시도
@@ -42,20 +45,20 @@ def on_disconnect(client, userdata, rc):
                 client.reconnect()
                 client.subscribe(TOPIC_SUB, qos=1)  # 재연결 후 구독 재설정
                 session_active = True
-                print("재연결 및 구독 재설정 성공")
+                logger.info("재연결 및 구독 재설정 성공")
             except Exception as e:
                 reconnect_attempts += 1
-                print(f"재연결 실패 ({reconnect_attempts}/5): {e}")
+                logger.warn(f"재연결 실패 ({reconnect_attempts}/5): {e}")
                 time.sleep(2)
     else:
-        print("정상적으로 연결이 종료되었습니다.")
+        logger.info("정상적으로 연결이 종료되었습니다.")
         session_active = False
 
 def on_message(client, userdata, message):
     global session_active, last_response_time, response_received
 
     if not session_active:
-        print("세션이 끊겨 메시지를 받을 수 없습니다.")
+        logger.error("세션이 끊겨 메시지를 받을 수 없습니다.")
         return
     
     print("on_message 트리거됨") 
@@ -77,11 +80,11 @@ def on_message(client, userdata, message):
                     global last_response_time, response_received
 
                     audio = AudioSegment.from_wav("response.wav")
-                    print("음성 파일 재생 시작")
+                    logger.info("음성 파일 재생 시작")
                     playback_in_progress.set()  # 재생 중 상태 설정
                     play(audio)
                     playback_in_progress.clear()  # 재생 중 상태 해제
-                    print("음성 파일 재생 완료")
+                    logger.info("음성 파일 재생 완료")
                     os.remove("response.wav")
                     # 응답 시간 갱신 및 이벤트 설정
                     last_response_time = time.time()
@@ -93,13 +96,13 @@ def on_message(client, userdata, message):
                 play_thread.start()
 
             except Exception as e:
-                print("PCM 데이터 처리 중 오류:", e)
+                logger.error("PCM 데이터 처리 중 오류:", e)
         else:
-            print("유효한 'audio' 필드가 메시지에 없습니다.")
+            logger.info("유효한 'audio' 필드가 메시지에 없습니다.")
     except json.JSONDecodeError:
-        print("메시지 페이로드의 JSON 디코딩에 실패했습니다.")
+        logger.error("메시지 페이로드의 JSON 디코딩에 실패했습니다.")
     except Exception as e:
-        print("on_message 오류:", e)
+        logger.error("on_message 오류:", e)
 
 
 def enqueue_message(header, data=None):
@@ -109,17 +112,18 @@ def enqueue_message(header, data=None):
     }
     if data:
         msg["data"] = data
-    message_queue.put(msg)  
-    print(f"메시지 큐에 추가됨: {msg}")
+    message_queue.put(msg)
+    logger.info(f"메시지 큐에 추가됨: {msg}")
+    # print(f"메시지 큐에 추가됨: {msg}")
 
 def message_publisher():  
     while True:
         msg = message_queue.get()  # 큐에서 메시지 가져오기
         try:
             result = client.publish(TOPIC_PUB, json.dumps(msg), qos=1)
-            print(f"메시지 발행 결과: {result.rc}, MID: {result.mid}")
+            logger.info(f"메시지 발행 결과: {result.rc}, MID: {result.mid}")
         except Exception as e:
-            print(f"메시지 발행 중 오류 발생: {e}")
+            logger.error(f"메시지 발행 중 오류 발생: {e}")
         message_queue.task_done()
 
 # 메시지 발행 스레드 시작
@@ -130,7 +134,7 @@ publisher_thread.start()
 def start_conversation():
     global last_response_time, response_received, conversation_active
     while not exit_event.is_set():
-        print("텍스트 받는 중")
+        print("텍스트 받는 중...")
 
         text = ""
         idle_time = 0
@@ -138,7 +142,7 @@ def start_conversation():
         while not text and not exit_event.is_set():
             # 오디오 재생 중일 경우 대기
             while playback_in_progress.is_set():
-                print("오디오 출력 중... 마이크 입력 대기")
+                # print("오디오 출력 중... 마이크 입력 대기")
                 time.sleep(0.1)  # 재생이 끝날 때까지 짧게 대기
 
             text = speech_to_text()  # 오디오 재생이 종료되면 마이크 입력 활성화
@@ -213,8 +217,9 @@ async def check_wake_word():
 
                 # MQTT 메시지 전송
                 enqueue_message("topic/message/send", data={"content": "토키야"})
-                print("MQTT 메시지 전송 완료: 토키야")
-                # TODO : 첫 응답 받기 전에 start_conversation 막기 위한 버퍼 (시간 설정 해보기)
+                logger.info("MQTT 메시지 전송 완료: 토키야")
+
+                # 첫 응답 받기 전에 start_conversation 막기 위한 버퍼 (시간 설정 해보기)
                 time.sleep(5)
 
                 # MQTT 전송이 완료된 후 대화 시작
@@ -240,18 +245,19 @@ async def main():
 client = mqtt.Client(client_id=CLIENT_ID, clean_session=False, protocol=getattr(mqtt, PROTOCOL))
 client.on_disconnect = on_disconnect
 client.on_message = on_message
-print("콜백 함수 등록 완료")
+logger.info("콜백 함수 등록 완료")
 
 # 브로커에 연결
 client.connect(BROKER_ADDRESS, keepalive=30)
-print("브로커에 연결 완료")
+logger.info("브로커에 연결 완료")
 
 # 구독 설정
 client.subscribe(TOPIC_SUB, qos=1)
-print(f"주제 {TOPIC_SUB} 구독 완료")
+logger.info(f"주제 {TOPIC_SUB} 구독 완료")
 
 # 이벤트 루프 시작
 client.loop_start()
+logger.info("MQTT 이벤트 루프 시작")
 print("MQTT 이벤트 루프 시작")
 
 if __name__ == "__main__":
@@ -261,9 +267,9 @@ if __name__ == "__main__":
         loop.close()
     except KeyboardInterrupt:
         enqueue_message("topic/conversation/end")  
-        print("프로그램 종료")
+        logger.info("프로그램 종료")
         exit_event.set()
     finally:
         client.loop_stop()
         client.disconnect()
-        print("프로그램이 종료되었습니다.")
+        logger.info("프로그램이 종료되었습니다.")
